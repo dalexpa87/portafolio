72954298b65839bdfe01ac4681a7f9f7c8c9248e
dalexpa87 <dalexpa@misena.edu.co>
2016-07-29 13:17:12 -0500

    App

---
+++ index.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="Es">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>Portafolio Cultural</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link rel="stylesheet" href="plugins/vegas/vegas.min.css">
+  
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+           
+      
+</head>
+<body class="grey lighten-5">
+
+<div class="navbar-fixed">
+  <nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#" class="brand-logo"><img src="img/logo.png" style="width:160px"></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#q">Quienes Somos?</a></li>        
+        
+          <ul id="dropdown1" class="dropdown-content">
+            <li><a href="#ce">Campañas Educativas</a></li>
+            <li class="divider"></li>
+            <li><a href="#!">Recorridos pedagógicos y Experiencias de Ciudad</a></li>
+            <li class="divider"></li>
+            <li><a href="#!">Talleres Artísticos</a></li>
+          </ul>
+          <ul id="dropdown2" class="dropdown-content">
+            <li><a href="#ce">Campañas Educativas</a></li>
+            <li class="divider"></li>
+            <li><a href="#!">Recorridos pedagógicos y Experiencias de Ciudad</a></li>
+            <li class="divider"></li>
+            <li><a href="#!">Talleres Artísticos</a></li>
+          </ul>
+          <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Portafolio de Servicios<i class="material-icons right">arrow_drop_down</i></a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#Q">Quienes Somos?</a></li>        
+        <li><a href="#" class="dropdown-button" href="#!" data-activates="dropdown2">Portafolio de servicio</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+  </div>
+  <span class="ir-arriba icon-arrow-up2"><i class="large material-icons" style="font-size: 2rem">navigation</i></span>
+ 
+
+ 
+
+  <div class="slider ">
+    <ul class="slides">
+      <li>
+        <img src="img/bycicle.jpg" id="img_slide"> <!-- random image -->
+        <div  class="caption center-align aclarar">
+          <h1 class="red-text" >Bienvenido</h1>
+          <h3 class=" black-text ">Estamos seguros que hacemos la diferencia, Conócenos</h3>
+        </div>
+      </li>
+      <li>
+        <img src="img/Montaj_Art.jpg" id="img_slide"> <!-- random image -->
+        <div class="caption left-align aclarar">
+          <h2 class="red-text"> Talleres Artísticos</h2>
+          <h3 class="black-text">Nuevas Experiencias</h3>
+        </div>
+      </li>
+      <li>
+        <img src="img/Caminata.jpg" id="img_slide" > <!-- random image -->
+        <div class="caption right-align aclarar ">
+          <h2 class="red-text" >Recorridos Pedagógicos</h2>
+          <h3 class="black-text">Formando Ciudadanos</h3>
+        </div>
+      </li>
+      <li>
+        <img src="img/Talleres.jpg" id="img_slide"> <!-- random image -->
+        <div class="caption right-align aclarar ">
+          <h2 class="red-text" >Campañas Educativas</h2>
+          <h3 class="black-text">Construímos Sociedad</h3>
+        </div>
+      </li>
+    </ul>
+  </div>
+  
+  
+    <div class="section" id="q">
+      <div class="row">
+          <div class="col s12 m6">
+            <h3 class="black-text left-align" >¿Quienes Somos? </h3>
+            
+            <p align="justify" id="p_section">Somos una empresa de gestión cultural y educativa integrada por un grupo multidisciplinario de personas altamente calificadas, entre ellos investigadores, gestores culturales y docentes en distintas áreas con quienes diseñamos diversas formas de promoción y difusión de la cultura en nuestro medio a través de la programacion de proyectos alternativos en el área educativa, artística y cultural.</p>
+          </div>
+
+        
+            <div class="col s12 m6">
+              <img src="img/1.jpg" id="imgsection">
+            </div>
+
+      </div>
+       
+      </div>
+
+    
+ 
+
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center aclarar">
+          <h5 class="header col s12 red-text light">El Ser Humano eje de nuestras acciones</h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="img/libro.jpg" alt="Unsplashed background libro"></div>
+  </div>
+
+   <div class="section">
+
+      <!--   Icon Section   -->
+      <div class="row">
+          
+           <div class="col s12 m6">
+              <img src="img/CampañaEducativa.jpg" id="imgsection">
+            </div>
+        <div class="col s12 m6">
+
+            <h3 class="black-text right-align" id="ce">Campañas Educativas</h3>
+            
+            <p align="justify" id="p_section">A través de puestas en escena, sensibilizamos y reflexionamos sobre: Educación Ambiental, Programas de Convivencia, Literatura e Historia, Comunicación Asertiva, Identidad y Diversidad en el aula, Acoso escolar o Bullyng.</p>
+          </div>
+      </div>
+  </div>
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center aclarar">
+          <h5 class="col s12 red-text light">No esperes mas para integrarte </h5>
+          <h4 class="col s12 red-text light"> y Vivir nuestra experiencia</h4>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="img/Foto2.jpg " alt="Unsplashed background img 3"></div>
+  </div>
+
+  <div class="container">
+    <div class="section">
+
+      <div class="row">
+        <div class="col s12 center">
+          <h3><i class="mdi-content-send brown-text"></i></h3>
+          <h4>Contactanos</h4>
+          <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+  
+  <!--Redes Sociales
+  <div class="caption">
+    <a  href='https://twitter.com/portacultural' style='display:scroll;position:fixed;bottom:330px;right:0px;' target='_blank'><img border='0' src='http://2.bp.blogspot.com/-r2kTvDo3bfw/Toc-MKnFfxI/AAAAAAAAA3A/3f86LWBmR38/s1600/boton%252520twitter.png' title='Seguirnos en Twitter'/></a>
+<a href='www.facebook.com/pages/Grupo-Portafolio-Cultural/310016442452318?fref=ts' style='display:scroll;position:fixed;bottom:185px;right:0px;' target='_blank'><img border='0' src='http://1.bp.blogspot.com/-7VSvlkal0os/UFORwVAFAZI/AAAAAAAAHkg/nhegYirxh5g/s1600/boton+facebook_opt.png' title='Seguinos en Facebook'/></a>
+<a href='URL_De_Google+' style='display:scroll;position:fixed;bottom:41px;right:0px;' target='_blank'><img border='0' src='http://4.bp.blogspot.com/-idti1v7hB8w/UFN_GPFp2NI/AAAAAAAAHjA/16kXdqvAXHQ/s1600/aizumgoogle+.png' title='Seguirnos en Google+'/></a>
+  </div>-->
+
+  <footer class="page-footer teal white">
+    <div class="container">
+      <div class="row ">
+          <div  class="col s12 m4">     
+           <p>Teléfono: 411 16 76</p>
+          <p >Dirección: Carrera 80 n° 37-18</p>
+          <p>Barrio Laureles, Medellín</p>
+      </div>
+      <div  class="col s12 m4">
+        <p class="simple-image" ;">
+        <img width="263" height="116" src="img/logo.png" class="attachment-full size-full" alt="logoportafolio"> </p>
+     </div> 
+     <div  class="col s12 m4">
+          <h3>Siguenos</h3> 
+           <div class="social-widg-cont">   
+               <a href="https://www.facebook.com/pages/Grupo-Portafolio-Cultural/310016442452318?fref=ts" target="_blank" title="Facebook">
+          <img src="http://portafoliocultural.org/wp-content/themes/weddings/images/fb.jpg" alt="">
+         </a>
+         
+                
+                <a href="https://twitter.com/portacultural" target="_blank" title="Twitter">
+          <img src="http://portafoliocultural.org/wp-content/themes/weddings/images/tw.jpg" alt="">
+         </a>
+         
+                
+                <a s href="#" target="_blank" title="Rss">
+          <img src="http://portafoliocultural.org/wp-content/themes/weddings/images/rss.jpg" alt=""></a>       
+           <a href="#" target="_blank" title="Google+">
+          <img src="http://portafoliocultural.org/wp-content/themes/weddings/images/googleplus.jpg" alt=""></a>
+         <a href="https://www.facebook.com/pages/Grupo-Portafolio-Cultural/310016442452318?fref=ts" target="_blank" title="Instagram" ><img src="http://portafoliocultural.org/wp-content/themes/weddings/images/instagram.jpg" alt=""></a>
+         <a href="https://www.facebook.com/pages/Grupo-Portafolio-Cultural/310016442452318?fref=ts" target="_blank" title="Youtube"><img src="http://portafoliocultural.org/wp-content/themes/weddings/images/youtube.jpg" alt="">
+         </a>       
+      </div>                      
+    </div>          
+    </div> 
+
+       
+        
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="brown-text text-lighten-3" href="#">David Pabón, Yohanny Lopez</a>
+      </div>
+    </div>
+    </div>
+  </footer>
+
+
+  <!--  Scripts-->
+  <script type="text/javascript" src="jquery.min.js"></script>
+      <script type="text/javascript" src="js/materialize.js"></script>
+      <script type="text/javascript" src="plugins/vegas/vegas.min.js"></script>
+
+      <script type="text/javascript">
+        $(document).ready(function(){
+
+        
+
+        $('.parallax').parallax();
+        $('.slider').slider({
+           height:  750,
+           indicators: false,
+            interval: 4000,
+            transition: 700,
+                              });
+        $('.ir-arriba').click(function(){
+        $('body, html').animate({
+        scrollTop: '0px'
+        }, 300);
+        });
+ 
+        $(window).scroll(function(){
+          if( $(this).scrollTop() > 0 ){
+         $('.ir-arriba').slideDown(600);
+    } else {
+      $('.ir-arriba').slideUp(300);
+    }
+  });
+        $(".button-collapse").sideNav();
+        $(".dropdown-button").dropdown();
+      });
+
+      </script>
+
+  </body>
+</html>
