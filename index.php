1156fd7484652df9f58f33e6151e878f36ce9124
dalexpa87 <dalexpa@misena.edu.co>
2016-07-29 17:06:13 -0500

    Cambios

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link rel="stylesheet" href="plugins/vegas/vegas.min.css">
   
+  <link rel="icon" type="image/png" href="img/favicon.png"/>
+  
+
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
            
@@ -31,14 +34,14 @@
             <li class="divider"></li>
             <li><a href="#!">Recorridos pedagógicos y Experiencias de Ciudad</a></li>
             <li class="divider"></li>
-            <li><a href="#!">Talleres Artísticos</a></li>
+            <li><a href="#ta">Talleres Artísticos</a></li>
           </ul>
           <ul id="dropdown2" class="dropdown-content">
             <li><a href="#ce">Campañas Educativas</a></li>
             <li class="divider"></li>
             <li><a href="#!">Recorridos pedagógicos y Experiencias de Ciudad</a></li>
             <li class="divider"></li>
-            <li><a href="#!">Talleres Artísticos</a></li>
+            <li><a href="#ta">Talleres Artísticos</a></li>
           </ul>
           <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Portafolio de Servicios<i class="material-icons right">arrow_drop_down</i></a></li>
       </ul>
@@ -63,28 +66,28 @@
         <img src="img/bycicle.jpg" id="img_slide"> <!-- random image -->
         <div  class="caption center-align aclarar">
           <h1 class="red-text" >Bienvenido</h1>
-          <h3 class=" black-text ">Estamos seguros que hacemos la diferencia, Conócenos</h3>
+          <h5 class=" black-text ">Estamos seguros que hacemos la diferencia, Conócenos</h5>
         </div>
       </li>
       <li>
         <img src="img/Montaj_Art.jpg" id="img_slide"> <!-- random image -->
         <div class="caption left-align aclarar">
           <h2 class="red-text"> Talleres Artísticos</h2>
-          <h3 class="black-text">Nuevas Experiencias</h3>
+          <h5 class="black-text">Nuevas Experiencias</h5>
         </div>
       </li>
       <li>
         <img src="img/Caminata.jpg" id="img_slide" > <!-- random image -->
         <div class="caption right-align aclarar ">
           <h2 class="red-text" >Recorridos Pedagógicos</h2>
-          <h3 class="black-text">Formando Ciudadanos</h3>
+          <h5 class="black-text">Formando Ciudadanos</h5>
         </div>
       </li>
       <li>
         <img src="img/Talleres.jpg" id="img_slide"> <!-- random image -->
         <div class="caption right-align aclarar ">
           <h2 class="red-text" >Campañas Educativas</h2>
-          <h3 class="black-text">Construímos Sociedad</h3>
+          <h5 class="black-text">Construímos Sociedad</h5>
         </div>
       </li>
     </ul>
@@ -94,7 +97,7 @@
     <div class="section" id="q">
       <div class="row">
           <div class="col s12 m6">
-            <h3 class="black-text left-align" >¿Quienes Somos? </h3>
+            <h5 class="black-text left-align" >¿Quienes Somos? </h5>
             
             <p align="justify" id="p_section">Somos una empresa de gestión cultural y educativa integrada por un grupo multidisciplinario de personas altamente calificadas, entre ellos investigadores, gestores culturales y docentes en distintas áreas con quienes diseñamos diversas formas de promoción y difusión de la cultura en nuestro medio a través de la programacion de proyectos alternativos en el área educativa, artística y cultural.</p>
           </div>
@@ -108,22 +111,18 @@
        
       </div>
 
-    
- 
-
-
   <div class="parallax-container valign-wrapper">
     <div class="section no-pad-bot">
       <div class="container">
         <div class="row center aclarar">
-          <h5 class="header col s12 red-text light">El Ser Humano eje de nuestras acciones</h5>
+          <h4 class="header col s12 red-text light">El Ser Humano eje de nuestras acciones</h4>
         </div>
       </div>
     </div>
     <div class="parallax"><img src="img/libro.jpg" alt="Unsplashed background libro"></div>
   </div>
 
-   <div class="section">
+   <div class="section" id="ce">
 
       <!--   Icon Section   -->
       <div class="row">
@@ -133,7 +132,7 @@
             </div>
         <div class="col s12 m6">
 
-            <h3 class="black-text right-align" id="ce">Campañas Educativas</h3>
+            <h5 class="black-text right-align" id="ce">Campañas Educativas</h5>
             
             <p align="justify" id="p_section">A través de puestas en escena, sensibilizamos y reflexionamos sobre: Educación Ambiental, Programas de Convivencia, Literatura e Historia, Comunicación Asertiva, Identidad y Diversidad en el aula, Acoso escolar o Bullyng.</p>
           </div>
@@ -152,13 +151,60 @@
     <div class="parallax"><img src="img/Foto2.jpg " alt="Unsplashed background img 3"></div>
   </div>
 
+  <div class="section" id="ta">
+
+      <!--   Icon Section   -->
+      <div class="row">
+          
+           
+        <div class="col s12 m6">
+
+            <h5 class="black-text right-align">Talleres Artísticos</h5>
+            
+            <p align="justify" id="p_section">Bajo una metodologia activa y participativa desarrollamos los siguientes programas: Danza, Teatro, Ballet, Caricatura, Pintura, Música.</p>
+          </div>
+        <div class="col s12 m6">
+              <img src="img/ArtePx.jpg" id="imgsection">
+            </div>
+      </div>
+  </div>
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center aclarar">
+          
+          <h4 class="col s12 red-text light"> Arte, Educación y Cultura</h4>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="img/imgpx.jpg " alt="Unsplashed background img 3"></div>
+  </div>
+    <div class="section" id="rc">
+
+      <!--   Icon Section   -->
+      <div class="row">
+          
+           
+        <div class="col s12 m6">
+
+            <h5 class="black-text right-align">Recorridos pedagógicos y experiencias de ciudad</h5>
+            
+            <p align="justify" id="p_section">Desde un encuentro sensible con la ciudad, tocamos los siguientes temas: Arte, y Arquitectura Pública, Mujer y Diversidad de Género, Migración y Transformación Urbana</p>
+          </div>
+        <div class="col s12 m6">
+              <img src="img/Recorrido.jpg" id="imgsection">
+            </div>
+      </div>
+  </div>
+
+
   <div class="container">
     <div class="section">
 
       <div class="row">
         <div class="col s12 center">
-          <h3><i class="mdi-content-send brown-text"></i></h3>
-          <h4>Contactanos</h4>
+          
+          <h5>Contáctanos</h5>
           <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
         </div>
       </div>
@@ -188,7 +234,7 @@
         <img width="263" height="116" src="img/logo.png" class="attachment-full size-full" alt="logoportafolio"> </p>
      </div> 
      <div  class="col s12 m4">
-          <h3>Siguenos</h3> 
+          <h5>Síguenos</h5> 
            <div class="social-widg-cont">   
                <a href="https://www.facebook.com/pages/Grupo-Portafolio-Cultural/310016442452318?fref=ts" target="_blank" title="Facebook">
           <img src="http://portafoliocultural.org/wp-content/themes/weddings/images/fb.jpg" alt="">
@@ -213,9 +259,9 @@
 
        
         
-    <div class="footer-copyright">
+    <div class="footer-copyright black" >
       <div class="container">
-      Made by <a class="brown-text text-lighten-3" href="#">David Pabón, Yohanny Lopez</a>
+      Made by <a class="white-text" style="font-size: 12px">HardBlack 2016 hardblack@outlook.es</a>
       </div>
     </div>
     </div>
