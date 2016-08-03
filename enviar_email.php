b81dd5d77a4c8c65f58e6fe1eb014321349ef0dc
Andrea0906 <saorozco17@misena.edu.co>
2016-08-02 19:18:11 -0500

    :+1:

--- enviar_email.php
+++ enviar_email.php
@@ -1,24 +1,33 @@
 <?php
+//$nombre = $_POST['nombre'];
+//$apellido = $_POST['apellido'];
+//$asunto = $_POST['asunto'];
+//$email = $_POST['email'];
+//$mensaje = $_POST['mensaje'];
+
+//$para = 'info@portafoliocultural.org';
+//$titulo = 'Mensaje de la web';
+//$header = 'From: ' . $email;
+//$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
+
+//if ($_POST['submit']) {
+//if (mail($para, $titulo, $msjCorreo, $header)) {
+//echo "<script language='javascript'>
+//alert('Mensaje enviado, muchas gracias.');
+//window.location.href = 'http://www.portafoliocultural.org';
+//</script>";
+//} else {
+//echo 'Falló el envio';
+
+//}
+//}
+$destino="el msjCorreo";
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
-$asunto = $_POST['asunto'];
 $email = $_POST['email'];
+$asunto = $_POST['asunto'];
 $mensaje = $_POST['mensaje'];
-
-$para = 'info@portafoliocultural.org';
-$titulo = 'Mensaje de la web';
-$header = 'From: ' . $email;
-$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
-
-if ($_POST['submit']) {
-if (mail($para, $titulo, $msjCorreo, $header)) {
-echo "<script language='javascript'>
-alert('Mensaje enviado, muchas gracias.');
-window.location.href = 'http://www.portafoliocultural.org';
-</script>";
-} else {
-echo 'Falló el envio';
-
-}
-}
+$contenido = "Nombre : ".$nombre."apellido: ".$apellido."correo: ".$email. "asunto: ".$asunto."mensaje: ".$mensaje;
+mail($destino,"contacto",$contenido);
+header("location:index.php");
 ?>
